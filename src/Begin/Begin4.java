c80da892cf57b74997dff8566b296f78d51f08a3
Shavkat-Ibrokhimov <shavkatibrohimov0405@gmail.com>
2025-02-13 22:23:58 +0500

    first commit

---
+++ src/Begin/Begin4.java
@@ -0,0 +1,14 @@
+package Begin;
+
+import java.util.Scanner;
+
+public class Begin4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("d = ");
+        double d = scanner.nextDouble();
+        double Pi = 3.14;
+        double L = Pi * d;
+        System.out.print("L = " + L);
+    }
+}
