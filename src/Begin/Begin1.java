3b8fac83eda21adcdb274d1ec1d6ad1938600b09
Shavkat-Ibrokhimov <shavkatibrohimov0405@gmail.com>
2025-02-13 22:02:41 +0500

    first commit

---
+++ src/Begin/Begin1.java
@@ -0,0 +1,13 @@
+package Begin;
+
+import java.util.Scanner;
+
+public class Begin1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a = ");
+        int a = scanner.nextInt();
+        int P = 4 * a;
+        System.out.print("P = " + P);
+    }
+}
