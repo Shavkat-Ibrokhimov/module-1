c5c71afadf649a0378c50a7463f14bb66ae4dc60
Shavkat-Ibrokhimov <shavkatibrohimov0405@gmail.com>
2025-02-13 22:43:47 +0500

    first commit

---
+++ src/Begin/Begin12.java
@@ -0,0 +1,23 @@
+package Begin;
+
+import java.util.Scanner;
+
+public class Begin12 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("a = ");
+        double a = scanner.nextDouble();
+
+        System.out.print("a = ");
+        double b = scanner.nextDouble();
+
+        double c = Math.sqrt(a * a + b * b);
+        double P = a + b + c;
+
+        System.out.println("c = " + c);
+        System.out.println("P = " + P);
+
+    }
+}
---
+++ src/Begin/Begin39.java
@@ -0,0 +1,24 @@
+package Begin;
+
+import java.util.Scanner;
+
+public class Begin39 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("A = ");
+        int A = scanner.nextInt();
+
+        System.out.print("B = ");
+        int B = scanner.nextInt();
+
+        System.out.println(" ");
+        System.out.println(A + "x+" + B + "=0");
+        System.out.println(" ");
+
+        double x = (double) ((-1 * B) / A);
+        System.out.print("x = " + x);
+
+    }
+}
